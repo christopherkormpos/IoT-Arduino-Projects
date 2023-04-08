424d3716c2d0a26c0798ebff69a34aeb02217265
unknown <christopher.kormpos@gmail.com>
2023-04-08 16:27:55 +0300

    initial commit

---
+++ DHTSensor/DHTSensor.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+DHT dht;
+#define HIGH1 2
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t");
+
+  dht.setup(12); // data pin 2
+}
+
+void loop()
+{
+  
+  digitalWrite(HIGH1, HIGH);   
+  delay(dht.getMinimumSamplingPeriod());
+
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+  
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\n");
+}
+
